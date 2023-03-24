60751ca87be37850c487894da0005a28036c222f
0Vipin0 <vipinmalik941@gmail.com>
2023-03-24 19:29:37 +0530

    Added Horizontal Scrolling

--- cli/lib/editor.dart
+++ cli/lib/editor.dart
@@ -14,6 +14,7 @@ class Editor {
   int rows = 0;
   int columns = 0;
   int offsetY = 0;
+  int offsetX = 0;
   List<String> content = [];
 
   final CursorCoordinate coordinate = CursorCoordinate(x: 0, y: 0);
@@ -30,7 +31,8 @@ class Editor {
     console.setTextStyle(inverted: true);
     writeStatusBar();
     console.setTextStyle();
-    console.cursorPosition = Coordinate(coordinate.y - offsetY, coordinate.x);
+    console.cursorPosition =
+        Coordinate(coordinate.y - offsetY, coordinate.x - offsetX);
   }
 
   void writeStatusBar() {
@@ -46,7 +48,19 @@ class Editor {
       if (fileCoordinateY >= content.length) {
         console.write("~");
       } else {
-        console.write(content[fileCoordinateY]);
+        final String line = content[fileCoordinateY];
+        int lengthToWrite = line.length - offsetX;
+        if (lengthToWrite < 0) {
+          lengthToWrite = 0;
+        }
+        if (lengthToWrite > columns) {
+          lengthToWrite = columns;
+        }
+        if (lengthToWrite > 0) {
+          final String lineToWrite =
+              line.substring(offsetX, offsetX + lengthToWrite);
+          console.write(lineToWrite);
+        }
       }
       console.write("\r\n");
     }
@@ -62,6 +76,8 @@ class Editor {
       ControlCharacter.arrowRight,
       ControlCharacter.home,
       ControlCharacter.end,
+      ControlCharacter.pageUp,
+      ControlCharacter.pageDown,
     ]).contains(key.controlChar)) {
       moveCursor(key);
     }
@@ -85,7 +101,7 @@ class Editor {
         }
         break;
       case ControlCharacter.arrowRight:
-        if (coordinate.x < columns - 1) {
+        if (coordinate.x < content[coordinate.y].length - 1) {
           coordinate.x++;
         }
         break;
@@ -94,6 +110,21 @@ class Editor {
         break;
       case ControlCharacter.end:
         coordinate.x = columns - 1;
+        break;
+      case ControlCharacter.pageUp:
+        coordinate.y = offsetY;
+        for (int i = 0; i < rows - 1; i++) {
+          moveCursor(Key.control(ControlCharacter.arrowUp));
+        }
+        break;
+      case ControlCharacter.pageDown:
+        coordinate.y = offsetY + rows - 1;
+        if (coordinate.y > content.length) {
+          coordinate.y = content.length;
+        }
+        for (int i = 0; i < rows - 1; i++) {
+          moveCursor(Key.control(ControlCharacter.arrowDown));
+        }
         break;
       default:
     }
@@ -114,6 +145,12 @@ class Editor {
       offsetY = coordinate.y - rows;
     } else if (coordinate.y < offsetY) {
       offsetY = coordinate.y;
+    }
+
+    if (coordinate.x >= offsetX + columns) {
+      offsetX = coordinate.x - columns;
+    } else if (coordinate.x < offsetX) {
+      offsetX = coordinate.x;
     }
   }
 }
